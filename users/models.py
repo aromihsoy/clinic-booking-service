c94bcd477d62baa5de4e15d195b581d07908af92
aromihsoy <Palmite24@mail.ru>
2026-07-13 15:56:02 +0500

    feat: add custom User model with roles

--- users/models.py
+++ users/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+
+class User(AbstractUser):
+    class Role(models.TextChoices):
+        PATIENT = "PATIENT", "Пациент"
+        DOCTOR = "DOCTOR", "Врач"
+        ADMIN = "ADMIN", "Администратор"
+
+    role = models.CharField("Роль", max_length=20, choices=Role.choices, default=Role.PATIENT)
+
+    @property
+    def is_doctor(self) -> bool:
+        return self.role == self.Role.DOCTOR
+    
+    @property
+    def is_patient(self) -> bool:
+        return self.role == self.Role.PATIENT
+    
+    @property
+    def is_admin(self) -> bool:
+        return self.role == self.Role.ADMIN
